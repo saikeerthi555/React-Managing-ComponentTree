1ca3949d5309f1c0e41d5a8f3210fae6d918b688
saikeerthi555 <43855677+saikeerthi555@users.noreply.github.com>
2021-02-13 21:52:03 +0530

    deleteItem

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,18 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 
 function ToDoItem(props) {
-  const [isDone, setIsDone] = useState(false);
-
-  function handleClick() {
-    setIsDone((prevValue) => {
-      return !prevValue;
-    });
-  }
   return (
-    <div onClick={handleClick}>
-      <li style={{ textDecoration: isDone ? "line-through" : "none" }}>
-        {props.text}
-      </li>
+    <div onClick={props.onChecked}>
+      <li>{props.text}</li>
     </div>
   );
 }
